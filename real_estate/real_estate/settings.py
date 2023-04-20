1cadf002082ed046d092a6252a6936896ff2ee35
Romal Tillu <romaltillu@gmail.com>
2023-04-20 19:19:14 +0530

    Updated css and added media specs

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "real_estate/static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+#Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL =  "/media/"
